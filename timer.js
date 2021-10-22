0f8b247e5bda0badb2caa0187b4307387177847b
Gregory Karastergios <gregory@gregnk.com>
2021-10-22 19:34:37 -0400

    Moved timer JS code to a .js file

---
+++ timer.js
@@ -0,0 +1,25 @@
+function getTime() {
+	// Get the amount of time left
+	var currentDate = Date.now() / 1000; // The amount seconds since the epoch
+	var endDate = 2147483647;            // 19 January 2038, 03:14:07 UTC
+	var timeLeftRaw = endDate - currentDate;
+	
+	// Format and display the time
+	var seconds = timeLeftRaw << 0;
+	var minutes = seconds / 60 << 0;
+	var hours = minutes / 60 << 0;
+	var days = hours / 24 << 0;
+	
+	seconds %= minutes;
+	minutes %= hours;
+	hours %= days;
+	
+	document.getElementById("days").innerHTML = days;
+	document.getElementById("hours").innerHTML = hours;
+	document.getElementById("minutes").innerHTML = minutes;
+	document.getElementById("seconds").innerHTML = seconds;
+	
+	// Update in real time
+	// Refreshes every second
+	var timeout = setTimeout(getTime, 1000);
+}
